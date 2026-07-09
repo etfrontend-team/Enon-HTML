8228b928ff0c1ff7657fe555548a5cbfcc187407
etfrontend-team <frontendteam.encircle@gmail.com>
2026-07-09 13:33:17 +0530

    refactor : contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,16 @@
+import ContactMap from "@/sections/ContactMap";
+import ExploreCards from "@/sections/ExploreCards";
+import HeroBanner from "@/sections/HeroBanner";
+import MediaCtaCards from "@/sections/MediaCtaCards";
+
+export default function Contact() {
+
+    return (
+      <>
+        <HeroBanner />
+        <ContactMap />
+        <MediaCtaCards />
+        <ExploreCards />
+      </>
+    );
+}
